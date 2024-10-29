15ca7a1add971b9198ccb503881b13503fd15a1e
DanutPopa <popa.ion.danut@gmail.com>
2024-10-29 12:44:43 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Danut Popa on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
